7f3a35c788dac86f513b57787db16c78bbd24cda
DevMan3158 <e.giner@codeur.online>
2022-08-12 07:36:25 +0200

    cat - mort

--- project/www/src/Entity/Mort.php
+++ project/www/src/Entity/Mort.php
@@ -3,6 +3,8 @@
 namespace App\Entity;
 
 use App\Repository\MortRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: MortRepository::class)]
@@ -17,6 +19,14 @@ class Mort
     #[ORM\JoinColumn(nullable: false)]
     private ?user $user = null;
 
+    #[ORM\OneToMany(mappedBy: 'cat', targetEntity: CatMort::class)]
+    private Collection $catMorts;
+
+    public function __construct()
+    {
+        $this->catMorts = new ArrayCollection();
+    }
+
     public function getId(): ?int
     {
         return $this->id;
@@ -30,6 +40,36 @@ class Mort
     public function setUser(?user $user): self
     {
         $this->user = $user;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, CatMort>
+     */
+    public function getCatMorts(): Collection
+    {
+        return $this->catMorts;
+    }
+
+    public function addCatMort(CatMort $catMort): self
+    {
+        if (!$this->catMorts->contains($catMort)) {
+            $this->catMorts->add($catMort);
+            $catMort->setCat($this);
+        }
+
+        return $this;
+    }
+
+    public function removeCatMort(CatMort $catMort): self
+    {
+        if ($this->catMorts->removeElement($catMort)) {
+            // set the owning side to null (unless already changed)
+            if ($catMort->getCat() === $this) {
+                $catMort->setCat(null);
+            }
+        }
 
         return $this;
     }
