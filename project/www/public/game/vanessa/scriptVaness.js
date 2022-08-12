14cc7ce019098791da857d75baf5389b50a184e5
poukette58 <vanessa.geoffroid@infamail.com>
2022-08-12 15:56:07 +0200

    modif canvasbottom

--- project/www/public/game/vanessa/scriptVaness.js
+++ project/www/public/game/vanessa/scriptVaness.js
@@ -1,19 +1,7 @@
 let screenGame = document.getElementById("screenGame");
 
-var lastScrollTop = 0;
-screenGame.addEventListener("scroll", function () {
-    var st = screenGame.pageYOffset || document.documentElement.scrollTop;
-    console.log(st);
-    if (st > lastScrollTop) {
-        // downscroll code
-    } else {
-        // upscroll code
-    }
-    lastScrollTop = st <= 0 ? 0 : st;
-}, false);
-
-
 let canvas = document.createElement('canvas');
+
 canvas.width = 700;
 canvas.height = 20;
 screenGame.appendChild(canvas);
@@ -21,4 +9,21 @@ let ctx = canvas.getContext("2d");
 
 ctx.fillStyle = 'rgb(255, 165, 0)';
 ctx.fillRect(0, 0, 700, 20);
+
+var lastScrollTop = 0;
+screenGame.scrollTop = screenGame.scrollHeight;
+screenGame.addEventListener("scroll", function () {
+    var st = -20*screenGame.scrollTop;
+    console.log(st);
+    canvas.style.marginTop = st+'px';
+    if (st > lastScrollTop) {
+        
+    } else {
+        // upscroll code
+    }
+    lastScrollTop = st <= 0 ? 0 : st;
+}, false);
+
+
+
 
