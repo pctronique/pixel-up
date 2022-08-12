8ffcc7e50408abba7a5f369a24f5d15219748e8d
poukette58 <vanessa.geoffroid@infamail.com>
2022-08-12 10:25:03 +0200

    backgroun.js+div

--- project/www/public/game/js/class/Background.js
+++ project/www/public/game/js/class/Background.js
@@ -1,14 +1,37 @@
-class Background{
-    constructor(imgBack, imgBas){
+class Background {
+    constructor(imgBack, imgBas) {
         this.imgBack = imgBack;
         this.imgBas = imgBas;
     }
 
-    deplacement(){
-
+    deplacement() {
+        var lastScrollTop = 0;
+        element.addEventListener("scroll", function () {
+            var st = window.pageYOffset || document.documentElement.scrollTop;
+            if (st > lastScrollTop) {
+                // downscroll code
+            } else {
+                // upscroll code
+            }
+            lastScrollTop = st <= 0 ? 0 : st; // For Mobile or negative scrolling
+        }, false);
     }
 
-    screenBottom(){
+    screenBottom() {
+
+        let canvas = document.getElementById('ecran_joueur');
+        if (canvas.getContext) {
+            canvas.width = 700;
+            canvas.height=700;
+            screenGame.appendChild(canvas);
+            let ctx = canvas.getContext("2d");
+
+            ctx.fillStyle = 'rgb(255, 165, 0)';
+            ctx.fillRect();
+
+        }
+
+
 
     }
     setPlateformeFixe() {
