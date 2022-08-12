cdd074f31bd0967670e8466d89cd218c810475ee
DevMan3158 <e.giner@codeur.online>
2022-08-12 09:10:43 +0200

    Retour sans cat_id

--- project/www/src/Entity/CatMort.php
+++ project/www/src/Entity/CatMort.php
@@ -16,10 +16,6 @@ class CatMort
     #[ORM\Column(length: 255)]
     private ?string $nom = null;
 
-    #[ORM\ManyToOne(inversedBy: 'catMorts')]
-    #[ORM\JoinColumn(nullable: false)]
-    private ?mort $cat = null;
-
     public function getId(): ?int
     {
         return $this->id;
@@ -33,18 +29,6 @@ class CatMort
     public function setNom(string $nom): self
     {
         $this->nom = $nom;
-
-        return $this;
-    }
-
-    public function getCat(): ?mort
-    {
-        return $this->cat;
-    }
-
-    public function setCat(?mort $cat): self
-    {
-        $this->cat = $cat;
 
         return $this;
     }
