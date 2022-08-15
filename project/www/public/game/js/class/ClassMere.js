4cd6c7fa022e06455bc836ea92a421f46c0ff913
Ludovic NAULOT <pctronique@free.fr>
2022-08-15 07:15:21 +0200

    modif classmere

--- project/www/public/game/js/class/ClassMere.js
+++ project/www/public/game/js/class/ClassMere.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 class ClassMere{     /*nom fichier*/
     constructor(name){/*name=variable*/
         this.name=name;   /* déclare la variable de la class*/
@@ -12,17 +11,3 @@ class ClassMere{     /*nom fichier*/
 
 let classMere = new ClassMere('vanessa');  /*classMere = variable / pour objet de la class + recupère les valeurs des paramètres*/
 classMere.bonjour(); /*appelle la fonction*/
-=======
-class ClassMere {
-    constructor(name) {
-        this.name = name; // pour créer la variable de la classe
-        this.description = "tintin et milou";
-    }
-   bonjour() {
-       console.log("bonjour" + this.name);
-       console.log(this.description);
-    }
-}
-let classMere = new ClassMere("frederic");
-classMere.bonjour()
->>>>>>> fred
