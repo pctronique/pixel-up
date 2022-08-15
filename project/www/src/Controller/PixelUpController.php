25e1d35796b56a68e0c70eeab6e268dac57dbe71
loloxdev <loris_labarre@outlook.fr>
2022-08-15 12:37:06 +0200

    modif d'organisation des templates + css modifProf

--- project/www/src/Controller/PixelUpController.php
+++ project/www/src/Controller/PixelUpController.php
@@ -9,11 +9,31 @@ use Symfony\Component\Routing\Annotation\Route;
 class PixelUpController extends AbstractController
 {
     /**
-     * @Route("/pixel/up", name="app_pixel_up")
+     * @Route("/", name="app_pixel_up")
      */
     public function index(): Response
     {
-        return $this->render('pixel_up/index.html.twig', [
+        return $this->render('pixel_up/home.html.twig', [
+            'controller_name' => 'PixelUpController',
+        ]);
+    }
+
+    /**
+     * @Route("/profil", name="app_profil")
+     */
+    public function profil(): Response
+    {
+        return $this->render('pixel_up/profil.html.twig', [
+            'controller_name' => 'PixelUpController',
+        ]);
+    }
+
+    /**
+     * @Route("/updateProfil", name="app_modif_profil")
+     */
+    public function updateP(): Response
+    {
+        return $this->render('pixel_up/updateProfil.html.twig', [
             'controller_name' => 'PixelUpController',
         ]);
     }
