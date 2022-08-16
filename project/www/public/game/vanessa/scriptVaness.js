712d81fdddfb01b282b916133c99e0f15825acb4
poukette58 <vanessa.geoffroid@infamail.com>
2022-08-16 16:58:11 +0200

    modif script scrollbar

--- project/www/public/game/vanessa/scriptVaness.js
+++ project/www/public/game/vanessa/scriptVaness.js
@@ -1,7 +1,16 @@
 let screenGame = document.getElementById("screenGame");
 
-let canvas = document.createElement('canvas');
+let canvasBackground = document.createElement('canvas');
+canvasBackground.width = 700;
+canvasBackground.height = 1000;
+screenGame.appendChild(canvasBackground);
+let ctxBg = canvasBackground.getContext("2d");
 
+ctxBg.fillStyle = '#017472';
+ctxBg.fillRect(0, 0, 700, 1000);
+
+let canvas = document.createElement('canvas');
+canvas.id = "canvas";
 canvas.width = 700;
 canvas.height = 20;
 screenGame.appendChild(canvas);
@@ -13,8 +22,7 @@ ctx.fillRect(0, 0, 700, 20);
 var lastScrollTop = 0;
 screenGame.scrollTop = screenGame.scrollHeight;
 screenGame.addEventListener("scroll", function () {
-    var st = -20*screenGame.scrollTop;
-    console.log(st);
+    st = -1*((screenGame.scrollHeight-screenGame.scrollWidth)+(screenGame.offsetHeight-screenGame.scrollTop));
     canvas.style.marginTop = st+'px';
     if (st > lastScrollTop) {
         
