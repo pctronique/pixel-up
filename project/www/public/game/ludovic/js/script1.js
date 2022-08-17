b3d0e9426afcc68a9b5b207995e0140ff323d58f
Ludovic NAULOT <pctronique@free.fr>
2022-08-17 09:51:55 +0200

    petite modif

--- project/www/public/game/ludovic/js/script1.js
+++ project/www/public/game/ludovic/js/script1.js
@@ -2,13 +2,13 @@ if (window.Worker) {
     let game = new Game();
     game.setJoueur(350,948, 20,50);
     game.addBackground(700,1000);
-    game.getJoueur().addListenerPos("joueur-pos-x", "joueur-pos-y");
+    //game.getJoueur().addListenerPos("joueur-pos-x", "joueur-pos-y");
     game.afficher();
 
     let screenGame = document.getElementById("screenGame");
     screenGame.scrollTop = screenGame.scrollHeight;
 
-    game.start();
+    //game.start();
 
 } else {
     console.log("Your browser doesn't support web workers.");
