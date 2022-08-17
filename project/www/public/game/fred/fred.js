2ede8bdf4f155fb7ba2e2249d36461a519bd5262
frederic.gauthier89@gmail.com <frederic.gauthier89@gmail.com>
2022-08-17 09:28:26 +0200

    création suppression et ajout background

--- project/www/public/game/fred/fred.js
+++ project/www/public/game/fred/fred.js
@@ -1,82 +1,57 @@
+//déclaration des variables que l'on a besoin
 let screenGame = document.getElementById("screenGame");
 let ajouter = document.getElementById("ajouter");
-let elementEnfant = document.getElementById("childElement");
-let backgroundYellow = document.getElementById("backgroundYellow");
-backgroundYellow.width = 700;
-backgroundYellow.height = 1000;
-let ctxBackgroundYellow = backgroundYellow.getContext("2d");
-ctxBackgroundYellow.fillStyle = "yellow";
-ctxBackgroundYellow.fillRect(0, 0, 700, 1000);
 let nombre = 0;
-
-//var lastScrollTop = 0;
-screenGame.scrollTop = screenGame.scrollHeight;
-
 let scrollChangement = 0;
 	
 function ajouterBackground() {
+	// permet d'ajouter un nouveau canvas tout en définissant sa taille
 	let newcanvas = document.createElement('canvas');
-	
 	newcanvas.width = 700;
 	newcanvas.height = 1000;
-	screenGame.appendChild(newcanvas);
+
+	//retourne l'objet contexte de dessin du canvas
 	let ctx = newcanvas.getContext("2d");
 	
-	let parentDiv = elementEnfant.parentNode
-	parentDiv.insertBefore(newcanvas, elementEnfant.nextSibling)
+	//insère avant un nouveau canva et retourne le premier élément dans le screenGame
+	screenGame.insertBefore(newcanvas, screenGame.querySelector("canvas"));
+		 
 	if (nombre == 0) {
 		ctx.fillStyle = "red";
-		ctx.fillRect(0, 0, 700, 1000);
 		
 	} else if (nombre == 1) {
 		ctx.fillStyle = "blue";
-		ctx.fillRect(0, 0, 700, 1000);
-
 		
 	} else if (nombre == 2) {
 		ctx.fillStyle = "green";
-		ctx.fillRect(0, 0, 700, 1000);
-		nombre = -1;
 		
+		nombre = -1;
+	}
+	ctx.fillRect(0, 0, 700, 1000);
+
+	// si le nombre est supérieur à 2 on supprime le dernier enfant
+	if (screenGame.childElementCount > 2) {
+		screenGame.removeChild(screenGame.lastChild);
 	}
 	nombre++;
-	scrollChangement = nombre*526;
-}
-// ajouter.addEventListener("click", function () {
-// ajouterBackground();
-	
-// }
-	
-// );
-
-
-screenGame.onscroll = function () {
-	console.log(screenGame.scrollTop);
-	console.log(scrollChangement);
-
-	if (screenGame.scrollTop > scrollChangement -10 && screenGame.scrollTop < scrollChangement +10 ) {
-		ajouterBackground();	
-	}
-	
-	// if (screenGame.scroll(500, ajouterBackground(document.querySelector('#screenGame').scrollIntoView({
-	//  	behavior: 'smooth'
-	//   }))));
-	
-	// if (window.scroll(500, ajouterBackground(document.getElementById("screenGame"))));
-		
-	// let currentScrollPos = window.pageYOffset;
-	  
-	// if(screenGame.scrollTop > 500) {
-	// 	screenGame.style.display = 'none';
-	// 	}
-	// 	hideScroll = currentScrollPos;
 }
 
+screenGame.addEventListener(
+  "scroll",
+  function () {
+    let scrollHaut =
+      screenGame.scrollHeight / screenGame.childElementCount -
+      screenGame.offsetHeight;
+    if (screenGame.scrollTop < scrollHaut) {
+		ajouterBackground();
+    }
+  },
+  false
+  );
+
+//on appelle les fonctions à lancer
 ajouterBackground();
-
-// alert('supprimer')
-// noeud_parent = newdiv.parentNode
-// noeud_parent.removeChild(newdiv)
-// tmp = pnode.offsetTop
+ajouterBackground();
+screenGame.scrollTop = screenGame.scrollHeight;
 
 
