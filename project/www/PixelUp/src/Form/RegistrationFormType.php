e2484d7b101f22d461620005027d56331d4bbd32
DevMan3158 <e.giner@codeur.online>
2022-08-18 16:57:26 +0200

    Inscription et Connexion

--- project/www/PixelUp/src/Form/RegistrationFormType.php
+++ project/www/PixelUp/src/Form/RegistrationFormType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\FormBuilderInterface;
@@ -12,12 +13,13 @@ use Symfony\Component\Validator\Constraints\IsTrue;
 use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotBlank;
 
+
 class RegistrationFormType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('username')
+            ->add('username', TextType::class, array('label'=>'Identifiant :',))
             ->add('Conditions', CheckboxType::class, [
                 'mapped' => false,
                 'constraints' => [
