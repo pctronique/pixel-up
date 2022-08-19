c1ab35e09b27a74f43cb1a57a8add6905750bb78
frederic.gauthier89@gmail.com <frederic.gauthier89@gmail.com>
2022-08-19 16:58:42 +0200

    plateforme meduse , nacelle, background sous mer

--- project/www/public/game/fred/scriptImagesBackground.js
+++ project/www/public/game/fred/scriptImagesBackground.js
@@ -1,8 +1,18 @@
-let sousTerre = new BackgroundSousTerre("background",new Taille(1500, 170));
-//  sousTerre.afficher();
+let sousTerre = new BackgroundSousTerre("background",new Taille(300, 170));
+sousTerre.afficher();
+  
+let sousMer = new BackgroundSousMer("background",new Taille(300, 170));
+sousMer.afficher();
 
-let meduses = new PlateformeMeduses(new Taille(1500, 170));
+let meduses = new PlateformeMeduses(new Taille(300, 170));
 meduses.setPosition(new Position(0, 1350));
-meduses.setImage("meduses3.webp");
+meduses.setImage("./img/meduses2.jpg");
 meduses.setCanvasBackground("background");
 meduses.afficher();
+
+
+let nacelles = new PlateformeNacelles(new Taille(300, 170));
+nacelles.setPosition(new Position(0, 700));
+nacelles.setImage("./img/nacelle.jpg");
+nacelles.setCanvasBackground("background");
+nacelles.afficher();
