31595876e7b7a8a7b8f9396cb05ca25499e7ba1b
Ludovic NAULOT <pctronique@free.fr>
2022-08-22 07:05:57 +0200

    modif diagramme et assembler code js

--- project/www/public/game/js/class/Game.js
+++ project/www/public/game/js/class/Game.js
@@ -10,24 +10,24 @@ class Game {
     this.nbBackground = 0;
     this.idBackground = "background_game_0";
     this.idScreen = idScreen;
-
   }
 
   addBackground(tailleX, tailleY) {
-    
     let screenGame = document.getElementById(this.idScreen);
     // permet d'ajouter un nouveau canvas tout en définissant sa taille
     let newcanvas = document.createElement('canvas');
     newcanvas.width = tailleX;
     newcanvas.height = tailleY;
     newcanvas.id = this.idBackground;
+    let scrollMove = new ScrollMove(this.idScreen);
     //retourne l'objet contexte de dessin du canvas
     let ctx = newcanvas.getContext("2d");
-    
     //insère avant un nouveau canva et retourne le premier élément dans le screenGame
     screenGame.insertBefore(newcanvas, screenGame.querySelector("canvas"));
-    let background = new Background(this.idBackground, new Taille(tailleX, tailleY));
+    let background = new Background(this.idBackground, new Taille(tailleX, tailleY), scrollMove);
     this.backgrounds.push(background);
+    this.createBackground();
+    this.deleteBackground();
   }
 
   createBackground() {
@@ -38,13 +38,13 @@ class Game {
 
   }
 
-  deleteBackground(screenGame) {
-    
+  deleteBackground() {
+    let screenGame = document.getElementById(this.idScreen);
     // console.log(screenGame);
     if (screenGame.childElementCount > 2) {
       screenGame.removeChild(screenGame.lastChild);
       this.backgrounds.splice(0, 1);
-  }
+    }
 
   }
 
@@ -60,7 +60,7 @@ class Game {
   afficher() {
     for (let index = 0; index < this.backgrounds.length; index++) {
       const element = this.backgrounds[index];
-      if (element != undefined && this.joueur != undefined) {
+      if (element != undefined && this.joueur != undefined && index == 0) {
         this.joueur.setBackground(element);
         element.setJoueur(this.joueur);
         element.afficher();
