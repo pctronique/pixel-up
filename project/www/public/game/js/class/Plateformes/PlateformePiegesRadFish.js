5b7dfe7e4db7d308c5343057e0f82981ba26910d
frederic.gauthier89@gmail.com <frederic.gauthier89@gmail.com>
2022-08-26 13:31:44 +0200

    creation plateforme piege nuage, poisson, nacelles

---
+++ project/www/public/game/js/class/Plateformes/PlateformePiegesRadFish.js
@@ -0,0 +1,28 @@
+class PlateformePiegesRadFish extends PlateformeMobile{
+    constructor(taille = undefined, collisionHautStable = true) {
+        super(taille, collisionHautStable);
+        this.taille = new Taille(250,140);
+
+    }
+    action(enumCollision) {
+        if (enumCollision == EnumCollision.HAUT) {
+            return EnumAction.MORT;
+        }
+        return EnumAction.NULL;
+    }
+    typeMort() {
+        return EnumTypeMort.TOMBER;
+    }
+
+    afficher(canvas) {
+        this.deplacement();
+
+        //this.img;
+        this.imageSrc('./img/RadFish.png');
+        let piegesRadFish = new Images(this.img, this.pos, this.taille);
+        piegesRadFish.setObjetDuplique(this);
+        piegesRadFish.setBackground(this.background);
+        piegesRadFish.afficher(canvas);
+    }
+   
+}               
