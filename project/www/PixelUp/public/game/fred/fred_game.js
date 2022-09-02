5bdcd23d23f24bdef3f03c4e3446a761a556e34a
frederic.gauthier89@gmail.com <frederic.gauthier89@gmail.com>
2022-09-02 10:35:29 +0200

    mise en forme

--- project/www/PixelUp/public/game/fred/fred_game.js
+++ project/www/PixelUp/public/game/fred/fred_game.js
@@ -1,74 +1,100 @@
 let folderWorker = "./../js/worker/";
 if (window.Worker) {
-    //déclaration des variables que l'on a besoin
-    let backgroundTaille = new Taille(1000, 2500);
-    let game = undefined;
-    let nbGame = 0;
+  //déclaration des variables que l'on a besoin
+  let backgroundTaille = new Taille(1000, 2500);
+  let game = undefined;
+  let nbGame = 0;
 
-    let divGame = document.getElementById("game");
-    let idScreen = "screenGame_0";
-    let heightJoueur = 60;
-    //let heightJoueur = 47;
-    game = new Game("screenGame_0", tabConfigBackground);
-    game.setTailleBackground(backgroundTaille.x, backgroundTaille.y);
-    game.setTailleJoueur(18, heightJoueur);
-    //game.setTailleJoueur(47, heightJoueur);
-    game.keyGame(tabConfig.key.keySaut, tabConfig.key.keyGauche, tabConfig.key.keyDroite, tabConfig.key.keyCoucou);
-    game.keyGameDev(tabConfig.key.keyHaut, tabConfig.key.keyBas);
-    game.configSaut(tabConfig.sautTomber.hauteurSaut, tabConfig.sautTomber.millisecondeSaut, tabConfig.sautTomber.millisecondeTomber);
-    game.configDeplacement(tabConfig.deplacement.largeurDeplacement, tabConfig.deplacement.millisecondeDeplacement);
-    game.configCoucou(tabConfig.coucou.largeurCoucou, tabConfig.coucou.millisecondeCoucou);
-    //game.setProjectDev();
-    game.scoreId("score");
-    game.setIdTypeMort("game_typeMort");
-    game.addBackground();
-    game.addBackground();
-    game.setPosInitJoueur(backgroundTaille.x / 2, backgroundTaille.y - heightJoueur - 1);
-    game.afficher();
+  let divGame = document.getElementById("game");
+  let idScreen = "screenGame_0";
+  let heightJoueur = 60;
+  //let heightJoueur = 47;
+  game = new Game("screenGame_0", tabConfigBackground);
+  game.setTailleBackground(backgroundTaille.x, backgroundTaille.y);
+  game.setTailleJoueur(18, heightJoueur);
+  //game.setTailleJoueur(47, heightJoueur);
+  game.keyGame(
+    tabConfig.key.keySaut,
+    tabConfig.key.keyGauche,
+    tabConfig.key.keyDroite,
+    tabConfig.key.keyCoucou
+  );
+  game.keyGameDev(tabConfig.key.keyHaut, tabConfig.key.keyBas);
+  game.configSaut(
+    tabConfig.sautTomber.hauteurSaut,
+    tabConfig.sautTomber.millisecondeSaut,
+    tabConfig.sautTomber.millisecondeTomber
+  );
+  game.configDeplacement(
+    tabConfig.deplacement.largeurDeplacement,
+    tabConfig.deplacement.millisecondeDeplacement
+  );
+  game.configCoucou(
+    tabConfig.coucou.largeurCoucou,
+    tabConfig.coucou.millisecondeCoucou
+  );
+  //game.setProjectDev();
+  game.scoreId("score");
+  game.setIdTypeMort("game_typeMort");
+  game.addBackground();
+  game.addBackground();
+  game.setPosInitJoueur(
+    backgroundTaille.x / 2,
+    backgroundTaille.y - heightJoueur - 1
+  );
+  game.afficher();
 
-    let screenGame = document.getElementById("screenGame_0");
-    screenGame.scrollTop = screenGame.scrollHeight;
+  let screenGame = document.getElementById("screenGame_0");
+  screenGame.scrollTop = screenGame.scrollHeight;
 
-   
+  // fonction mathématique écoute évènement au scroll par rapport à la taille de screenGame
+  screenGame.addEventListener(
+    "scroll",
+    function () {
+      let calcul0 =
+        screenGame.scrollHeight -
+        screenGame.scrollWidth +
+        (screenGame.scrollWidth - screenGame.offsetHeight) -
+        screenGame.scrollTop;
+      let calcul4 = screenGame.scrollHeight / 2 / backgroundTaille.y;
+      let st =
+        -1 *
+        (game.backgrounds[0].scrollMove.placeBas() /
+          game.backgrounds[0].scrollMove.taillePixel());
+      console.log(calcul4);
+      game.screenBottom(st);
+      //game.afficher();
+      let scrollHaut = game.backgrounds[0].scrollMove.changeBackground();
+      if (screenGame.scrollTop < scrollHaut) {
+        game.addBackground(backgroundTaille.x, backgroundTaille.y);
+      }
+    },
+    false
+  );
 
-    // fonction mathématique écoute évènement au scroll par rapport à la taille de screenGame 
-    screenGame.addEventListener("scroll", function () {
-        let calcul0 = ((screenGame.scrollHeight - screenGame.scrollWidth) + (screenGame.scrollWidth - screenGame.offsetHeight) - screenGame.scrollTop);
-        let calcul4 = (screenGame.scrollHeight / 2) / backgroundTaille.y;
-        let st = -1 * (game.backgrounds[0].scrollMove.placeBas() / game.backgrounds[0].scrollMove.taillePixel());
-        console.log(calcul4);
-        game.screenBottom(st);
-        //game.afficher();
-        let scrollHaut = game.backgrounds[0].scrollMove.changeBackground();
-        if (screenGame.scrollTop < scrollHaut) {
-            game.addBackground(backgroundTaille.x, backgroundTaille.y);
-        }
-    }, false);
-
-    // getRandom();
-    // screenGame.scrollTop = screenGame.scrollHeight;
-   let loadSound = true;
-    function changer() {
-        if (loadSound) {
-            play();
-            document.getElementById("son_jeu").src = "img/haut_parleur_son_ouvert.png";
-            loadSound = false;
-        } else {
-            stop();
-            document.getElementById("son_jeu").src = "img/haut_parleur_son_coupe.png";
-            loadSound = true;
-            
-        }
+  // getRandom();
+  // screenGame.scrollTop = screenGame.scrollHeight;
+  let loadSound = true;
+  function changer() {
+    if (loadSound) {
+      play();
+      document.getElementById("son_jeu").src =
+        "img/haut_parleur_son_ouvert.png";
+      loadSound = false;
+    } else {
+      stop();
+      document.getElementById("son_jeu").src = "img/haut_parleur_son_coupe.png";
+      loadSound = true;
     }
-    function startGame(e) {
-        document.getElementById("info_Game").style.display = "none";
-        changer();
-        game.start();
-    }
+  }
+  function startGame(e) {
+    document.getElementById("info_Game").style.display = "none";
+    changer();
+    game.start();
+  }
 
- 
-document.getElementById("start").addEventListener("click", startGame);
-document.getElementById("son_jeu").addEventListener("click", changer);
+  document.getElementById("start").addEventListener("click", startGame);
+  document.getElementById("son_jeu").addEventListener("click", changer);
 
   document.querySelector("#button_up").addEventListener("click", function (e) {
     game.eventKey(" ");
--- project/www/PixelUp/public/game/js/audio.js
+++ project/www/PixelUp/public/game/js/audio.js
@@ -1,48 +1,44 @@
+var fileUrl = "./son/man-is-he-mega-glbml-22045.mp3";
 
-        var fileUrl = './son/man-is-he-mega-glbml-22045.mp3';
+window.AudioContext = window.AudioContext || window.webkitAudioContext;
+var audioContext = null;
+var audioSource = null;
 
-        window.AudioContext = window.AudioContext || window.webkitAudioContext;
-        var audioContext = null;
-        var audioSource = null;
-    
-        var loopTime = 31;
-    
-        function play() {
-    
-            //Create Audio context
-            if (!audioContext) {
-                audioContext = new AudioContext();
-            }
-    
-    
-            //Create Buffer source and connect it to "standard" audio destination ("speaker")
-            audioSource = audioContext.createBufferSource();
-            audioSource.connect(audioContext.destination);
-    
-    
-            //Load file
-            var request = new XMLHttpRequest();
-            request.open('GET', fileUrl, true);
-            request.responseType = 'arraybuffer';
-            request.onload = function () {
-                //Decode arrayBuffer as AudioData
-                audioContext.decodeAudioData(request.response, function (audioData) {
-                    audioSource.buffer = audioData;
-                    audioSource.loop = true;
-                    audioSource.loopEnd = loopTime;
-                    //Play "file"
-                    audioSource.start(0);
-                }, function () { console.error('The request failed.'); });
-            }
-            request.send();
-        }
-    
-        function stop() {
-            audioSource.stop(0);
-        }
-    
+var loopTime = 31;
 
+function play() {
+  //Create Audio context
+  if (!audioContext) {
+    audioContext = new AudioContext();
+  }
 
-    
+  //Create Buffer source and connect it to "standard" audio destination ("speaker")
+  audioSource = audioContext.createBufferSource();
+  audioSource.connect(audioContext.destination);
 
- 
+  //Load file
+  var request = new XMLHttpRequest();
+  request.open("GET", fileUrl, true);
+  request.responseType = "arraybuffer";
+  request.onload = function () {
+    //Decode arrayBuffer as AudioData
+    audioContext.decodeAudioData(
+      request.response,
+      function (audioData) {
+        audioSource.buffer = audioData;
+        audioSource.loop = true;
+        audioSource.loopEnd = loopTime;
+        //Play "file"
+        audioSource.start(0);
+      },
+      function () {
+        console.error("The request failed.");
+      }
+    );
+  };
+  request.send();
+}
+
+function stop() {
+  audioSource.stop(0);
+}
