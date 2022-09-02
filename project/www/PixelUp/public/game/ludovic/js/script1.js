0cd96555e9e90b6cad25d2ab4f122adb30c6ebcf
Ludovic NAULOT <pctronique@free.fr>
2022-09-02 13:10:57 +0200

    active auto scroll

--- project/www/PixelUp/public/game/ludovic/js/script1.js
+++ project/www/PixelUp/public/game/ludovic/js/script1.js
@@ -11,6 +11,7 @@ if (window.Worker) {
     let heightJoueur = 60;
     //let heightJoueur = 47;
     game = new Game("screenGame_0", tabConfigBackground, tabConfigMoveBackground);
+    game.activeAutoScroll(true);
     game.setVolumeEffet(0);
     game.setTailleBackground(backgroundTaille.x, backgroundTaille.y);
     game.setTailleJoueur(18, heightJoueur);
