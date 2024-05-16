a4e4d068f6b62fc2b8c5d81016c5dfa70135f523
pctronique <pctronique@free.fr>
2024-05-16 18:33:02 +0200

    del copy

--- www/src/Controller/JeuxController.php
+++ www/src/Controller/JeuxController.php
@@ -8,10 +8,34 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class JeuxController extends AbstractController
 {
-    #[Route('/jeux', name: 'app_jeux')]
+    #[Route('/game', name: 'app_jeux')]
     public function index(): Response
     {
         return $this->render('jeux/index.html.twig', [
+            'controller_name' => 'JeuxController',
+        ]);
+    }
+
+    #[Route('/game/ludovic', name: 'app_jeux_ludovic')]
+    public function ludovic(): Response
+    {
+        return $this->render('jeux/ludovic.html.twig', [
+            'controller_name' => 'JeuxController',
+        ]);
+    }
+
+    #[Route('/game/vanessa', name: 'app_jeux_vanessa')]
+    public function vanessa(): Response
+    {
+        return $this->render('jeux/vanessa.html.twig', [
+            'controller_name' => 'JeuxController',
+        ]);
+    }
+
+    #[Route('/game/fred', name: 'app_jeux_fred')]
+    public function fred(): Response
+    {
+        return $this->render('jeux/fred.html.twig', [
             'controller_name' => 'JeuxController',
         ]);
     }
