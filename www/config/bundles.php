d46205194acbadc3bdfe89e6f132e6f4bc636495
pctronique <pctronique@free.fr>
2025-03-21 09:58:29 +0100

    modif mailhog

--- www/config/bundles.php
+++ www/config/bundles.php
@@ -15,5 +15,4 @@ return [
     SymfonyCasts\Bundle\VerifyEmail\SymfonyCastsVerifyEmailBundle::class => ['all' => true],
     Knp\Bundle\PaginatorBundle\KnpPaginatorBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
-
 ];
